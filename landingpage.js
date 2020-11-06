2052dcec05dad108369fb2e53c6cf7e97a460ae6
truyentdm <truyentdm@gmail.com>
2020-11-06 14:08:29 +0700

    Create landingpage.js

---
+++ landingpage.js
@@ -0,0 +1,29 @@
+  	var custom = {
+    	"#header-wrap": "hide",
+      	"#sidebar-wrapper": "hide",
+      	"#footer-wrapper": "hide",
+      	"#main-wrapper": "fullsize",
+      	".post-header": "hide"
+    }
+    /*******************FUNC****************/
+    function addBGImage(idClass,url){
+    	$(idClass).css({"background-image":"url("+url+")"});
+    }
+    function addCss(idClass,objCss){
+		$(idClass).css(objCss);
+    }
+  	
+  	function createLandingPage(custom){
+    	if(typeof custom == "object"){
+        	for (const property in custom) {
+              $(property).addClass(custom[property]);
+            }
+        }else{
+          	//default
+        	$("#header-wrap").addClass('hide');     
+            $("#sidebar-wrapper").addClass('hide');
+            $("#footer-wrapper").addClass('hide');
+            $("#main-wrapper").addClass("fullsize");
+            $(".post-header").addClass('hide');
+        }
+    }
